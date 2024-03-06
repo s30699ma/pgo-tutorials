1b5b5ac75c1e5ba486e4b251514e95fcceeab833
Magdalena Bieńkowska <s30699@pjwstk.edu.pl>
2024-03-06 11:38:23 +0100

    Exercise 1

---
+++ pgo cw1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      int value = -5;
+      if(value > 0){
+            System.out.println("The number is positive");
+        }
+      else if(value < 0){
+          System.out.println("The number is negative");
+      }
+      else if(value == 0){
+          System.out.println("The number equals 0");
+        }
+    }
+}
